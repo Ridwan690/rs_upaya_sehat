e2e0db84873bb3cbbff4e3c871fc0a6c412d0bb2
tfajar123 <tfajar123123@gmail.com>
2024-06-26 12:29:57 +0700

    Tambah Dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,13 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Antrian;
+use App\Models\Kunjungan;
+use App\Models\RawatJalan;
+use App\Models\RawatInap;
+use Carbon\Carbon;
 
 class AdminController extends Controller
 {
-    // public function index()
-    // {
-    //     return view('admin.dashboard');
-    // }
+    public function index()
+    {
+        $today = Carbon::today();
+
+        $jumlahKunjungan = Kunjungan::whereDate('created_at', $today)->count();
+        $jumlahRawatJalan = RawatJalan::whereDate('created_at', $today)->count();
+        $jumlahRawatInap = RawatInap::whereDate('created_at', $today)->count();
+        $antrian = Antrian::whereDate('created_at', $today)->count();
+
+        return view('admin.dashboard', compact('antrian', 'jumlahKunjungan', 'jumlahRawatJalan', 'jumlahRawatInap'));
+    }
     // public function daftar()
     // {
     //     return view('form.daftar');
